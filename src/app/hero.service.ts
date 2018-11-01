2fa52834e8aba02dfea70527b243fd2d63ad7ec2
Miss Tiny <jingyi-yang@uiowa.edu>
2018-11-01 16:26:02 -0500

    code for routing service.
    add addition URL that linked to different component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,5 +13,9 @@ export class HeroService {
     this.messageService.add('HeroService:getched heroes');
     return of(HEROES);
   }
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add('HeroService: fetched hero id=${id}');
+    return of(HEROES.find(x => x.id === id));
+  }
   constructor(private messageService: MessageService) {}
 }
